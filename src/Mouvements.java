daea3724699c1d227c95be7bdacd3b31b4a5a54b
Abdoul193 <abdouldiallo50@outlook.fr>
2024-10-07 16:26:38 +0200

    commit initial

---
+++ src/Mouvements.java
@@ -0,0 +1,47 @@
+import lejos.hardware.BrickFinder;
+import lejos.hardware.lcd.GraphicsLCD;
+import lejos.hardware.motor.Motor;
+import lejos.utility.Delay;
+
+public class Mouvements {
+
+	private static final int SPEED = 500;
+
+	public  void avancer() {
+		Motor.B.setSpeed(SPEED);
+		Motor.D.setSpeed(SPEED);
+		Motor.B.forward();
+		Motor.D.forward();
+		Delay.msDelay(2000);
+	}
+
+	public  void reculer() {
+		Motor.B.setSpeed(SPEED);
+		Motor.D.setSpeed(SPEED);
+		Motor.B.backward();
+		Motor.D.backward();
+		Delay.msDelay(2000);
+	}
+	public  void tourner(int angle) {
+		int angleArr=(int) Math.round(4.44*angle);
+		Motor.D.rotate(angleArr); 
+		Delay.msDelay(2000); 
+	}
+	public  void fermerPince() {
+		Motor.C.setSpeed(SPEED); 
+		Motor.C.backward(); 
+		Delay.msDelay(1500);
+		
+	}
+	
+public  void ouvrirPince() {
+		Motor.C.setSpeed(SPEED);
+		Motor.C.forward();
+		Delay.msDelay(1500);
+}
+		
+
+	
+		    
+
+}
---
+++ src/Perception.java
@@ -0,0 +1,9 @@
+
+public class Perception {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
