f90d6f6e1f008deca69397431475029e3706de6f
roxdbs <roxane.dubus4@gmail.com>
2024-10-21 17:00:19 +0200

    Update Robot.java
    modifications avancerVers()

--- src/Robot.java
+++ src/Robot.java
@@ -23,15 +23,25 @@ public class Robot extends Perception {
         // avance en s'assurant d'aller dans la direction du palet
         // ouvre les pinces lorsque le palet est à moins de 0,9m 
         // distancePalet correspond à la distance minimale trouvée dans la méthode rechercherEtTourner()
-        float nouvelleDistancePalet;
-        avancer(distancePalet);
-		for(float i=0; i<distancePalet; i+=0.05) {
-            nouvelleDistancePalet=distance();
-            if (nouvelleDistancePalet>distancePalet) {
-                stopRobot();
-                recherche(220);
-            }
-        }
+        
+        //robot avance
+        Motor.B.forward(); 
+		Motor.D.forward(); 
 
+		float[] tabDistances = new float[30]; // Tableau pour enregistrer les distances avec le palet captées en avançant
+
+		// avancer et vérifier distance du palet qui diminue
+		for (int i=0; i < tabDistances.length || i<(int) distancePalet; i++) {
+			float nouvelleDistancePalet = distance();
+			tabDistances[i] = nouvelleDistancePalet;
+            if(i>0 && tabDistances[i]<tabDistances[i-1]) {
+                stopRobot();
+                rechercheEtTourner(220);
+            }
+			Delay.msDelay(111); // vérifie distance du palet tous les 5cm
+		}
+
+        stopRobot();
+        //gérer l'ouverture des pinces qd il s'approche du palet
 	}
 }
