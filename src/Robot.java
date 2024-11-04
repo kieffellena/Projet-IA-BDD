5a007563e480debc217563e4392ce88811ec2fe6
roxdbs <roxane.dubus4@gmail.com>
2024-11-04 15:12:20 +0100

    Update Robot.java
    ajout de la méthode avancerVers() finalisée

--- src/Robot.java
+++ src/Robot.java
@@ -22,26 +22,30 @@ public class Robot extends Perception {
     public void avancerVers(float distancePalet) {
         // avance en s'assurant d'aller dans la direction du palet
         // ouvre les pinces lorsque le palet est à moins de 0,9m 
-        // distancePalet correspond à la distance minimale trouvée dans la méthode rechercherEtTourner()
-        
-        //robot avance
-        Motor.B.forward(); 
-		Motor.D.forward(); 
+        // DistanceMinPalet correspond à la distance minimale trouvée dans la méthode recherche())
 
-		float[] tabDistances = new float[30]; // Tableau pour enregistrer les distances avec le palet captées en avançant
+        if (verifierDistance()) {
+            //robot avance
+            Motor.B.forward();
+            Motor.D.forward();
 
-		// avancer et vérifier distance du palet qui diminue
-		for (int i=0; i < tabDistances.length || i<(int) distancePalet; i++) {
-			float nouvelleDistancePalet = distance();
-			tabDistances[i] = nouvelleDistancePalet;
-            if(i>0 && tabDistances[i]<tabDistances[i-1]) {
-                stopRobot();
-                rechercheEtTourner(220);
+            float[] tabDistances = new float[30]; // Tableau pour enregistrer les distances avec le palet captées en avançant
+
+            // avancer et vérifier distance du palet qui diminue
+            for (int i = 0; i < tabDistances.length || i < (int) distancePalet; i++) {
+                float nouvelleDistancePalet = distance();
+                tabDistances[i] = nouvelleDistancePalet;
+                if (tabDistances[i]<=0.9) 
+                    ouvrirPince();
+                if (i > 0 && tabDistances[i] < tabDistances[i-1]) {
+                    stopRobot();
+                    recherche(220);
+                }
+                Delay.msDelay(111); // vérifie distance du palet tous les 5cm
             }
-			Delay.msDelay(111); // vérifie distance du palet tous les 5cm
-		}
+        }
 
-        stopRobot();
-        //gérer l'ouverture des pinces qd il s'approche du palet
-	}
+        fermerPince();
+        //méthode allerBase();
+    }
 }
