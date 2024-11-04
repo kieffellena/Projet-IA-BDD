84776b8e03ee118a1ac727ca3a8194ceb097cce8
roxdbs <roxane.dubus4@gmail.com>
2024-11-04 15:18:56 +0100

    Update Robot.java
    suppression du paramètre dans la méthode avancerVers() et utilisation de DistanceMinPalet 
    méthode recherche() lancée au début de la méthode avancerVers()

--- src/Robot.java
+++ src/Robot.java
@@ -1,3 +1,4 @@
+
 import lejos.hardware.Button;
 import lejos.hardware.Sound;
 import lejos.hardware.motor.Motor;
@@ -19,11 +20,12 @@ public class Robot extends Perception {
         mouvements = new Mouvements();
     }
 
-    public void avancerVers(float distancePalet) {
+    public void avancerVers() {
         // avance en s'assurant d'aller dans la direction du palet
         // ouvre les pinces lorsque le palet est à moins de 0,9m 
         // DistanceMinPalet correspond à la distance minimale trouvée dans la méthode recherche())
 
+        recherche(220);
         if (verifierDistance()) {
             //robot avance
             Motor.B.forward();
@@ -32,7 +34,7 @@ public class Robot extends Perception {
             float[] tabDistances = new float[30]; // Tableau pour enregistrer les distances avec le palet captées en avançant
 
             // avancer et vérifier distance du palet qui diminue
-            for (int i = 0; i < tabDistances.length || i < (int) distancePalet; i++) {
+            for (int i = 0; i < tabDistances.length || i < (int) DistanceMinPalet; i++) {
                 float nouvelleDistancePalet = distance();
                 tabDistances[i] = nouvelleDistancePalet;
                 if (tabDistances[i]<=0.9) 
