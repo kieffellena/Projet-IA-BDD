6065bff14d595029425dabfdd7aefb4dd5040b73
Abdoul193 <abdouldiallo50@outlook.fr>
2024-11-13 10:01:41 +0100

    Update Mouvements.java

--- src/Mouvements.java
+++ src/Mouvements.java
@@ -1,18 +1,11 @@
-
-import javax.management.monitor.Monitor;
 import lejos.hardware.BrickFinder;
 import lejos.hardware.lcd.GraphicsLCD;
 import lejos.hardware.motor.Motor;
 import lejos.utility.Delay;
-
 public class Mouvements {
 
 	protected static final int SPEED = 500;
-	private int compteurDeDegre;
-
-    public Mouvements() {
-        compteurDeDegre=0; //robot dirigé vers la zone en-but
-    }
+	public int compteurDeDegre=0;
 
 
 	//distance en cm
@@ -22,22 +15,21 @@ public class Mouvements {
 		Motor.D.setSpeed(SPEED);
 		Motor.B.forward();
 		Motor.D.forward();
-		int delay = (int) Math.round(d/50) ;
-		//2000 delay pr 40cm
+		int delay = (int) Math.round(d * 40);  // 40 ms par cm
 		Delay.msDelay(delay);
+
 	}
 
 	public void stopRobot() {
-		Motor.B.stop();
-		Motor.D.stop();
+		Motor.B.flt(true);
+		Motor.D.flt(true);
 	}
-
-	public  void reculer(double distance) {
+	public  void reculer(double d) {
 		Motor.B.setSpeed(SPEED);
 		Motor.D.setSpeed(SPEED);
 		Motor.B.backward();
 		Motor.D.backward();
-		int delay = (int) Math.round(distance/50) ;
+		int delay = (int) Math.round(d * 40);  // 40 ms par cm
 		Delay.msDelay(delay);
 	}
 
@@ -46,26 +38,26 @@ public class Mouvements {
 		int angleArr=(int) Math.round(4.44*angle);
 		Motor.D.rotate(angleArr); 
 		Delay.msDelay(2000); 
-        	compteurDeDegre+=angle;
+		compteurDeDegre+=angle;
 	}
 
-	public void tournerVersZoneEnBut() {
+	/*public void tournerVersZoneEnBut() {
 		tourner(-compteurDeDegre);
 		compteurDeDegre=0;
-	}
+	}*/
 
 	public  void fermerPince() {
-		Motor.C.setSpeed(SPEED); 
+		Motor.C.setSpeed(500); 
 		Motor.C.backward(); 
-		Delay.msDelay(1500);
+		Delay.msDelay(4000);
 	}
 
 	public  void ouvrirPince() {
-		Motor.C.setSpeed(SPEED); 
+		Motor.C.setSpeed(500); 
 		Motor.C.forward(); 
-		Delay.msDelay(1500);
+		Delay.msDelay(3000);
 	}
-	
+
 	public void efficaceTourner(int angle) {
 		//si +180 // - 180
 		// +90 - 90
@@ -77,5 +69,12 @@ public class Mouvements {
 			int nvAngle=360-angle;
 			tourner(nvAngle);
 		}
+	}
+	public static void main(String[] args) {
+		Mouvements M = new Mouvements();
+		//M.tourner(360);
+	//	M.ouvrirPince();
+	M.fermerPince();
+		//M.reculer(30);
 	}
 }
