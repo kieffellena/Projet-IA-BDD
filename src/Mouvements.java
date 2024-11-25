8e252b1fbfe525cb86f0058ce8c2d207824a15e1
kieffellena <kieffellena@gmail.com>
2024-11-25 14:36:33 +0100

    Update Mouvements.java
    Belle interface, tournerverszoneenbut, changer efficace tourner, intialisation des attributs dans le constructeur

--- src/Mouvements.java
+++ src/Mouvements.java
@@ -3,34 +3,32 @@ import lejos.hardware.lcd.GraphicsLCD;
 import lejos.hardware.motor.Motor;
 import lejos.utility.Delay;
 public class Mouvements {
+	//distance en cm
 
 	protected static final int SPEED = 500;
-	public int compteurDeDegre=0;
-	public boolean pinceOuverte=false;
+	public float compteurDeDegre;
+	public boolean pinceOuverte;
 
+	public Mouvements() {
+		compteurDeDegre=0;
+		pinceOuverte=false;
+	}
 
-	//distance en cm
-	//2000 delay pr 0,4cm
 	public void avancer(double d) { 
 		Motor.B.setSpeed(SPEED);
 		Motor.D.setSpeed(SPEED);
 		Motor.B.forward();
 		Motor.D.forward();
-		int delay = (int) Math.round(d * 40);  // 40 ms par cm
+		int delay = (int) Math.round(d * 40);
 		Delay.msDelay(delay);
-
 	}
 
-	public void stopRobot() {
-		Motor.B.flt(true);
-		Motor.D.flt(true);
-	}
 	public  void reculer(double d) {
 		Motor.B.setSpeed(SPEED);
 		Motor.D.setSpeed(SPEED);
 		Motor.B.backward();
 		Motor.D.backward();
-		int delay = (int) Math.round(d * 40);  // 40 ms par cm
+		int delay = (int) Math.round(d * 40);
 		Delay.msDelay(delay);
 	}
 
@@ -42,45 +40,39 @@ public class Mouvements {
 		compteurDeDegre+=angle;
 	}
 
-	/*public void tournerVersZoneEnBut() {
-		tourner(-compteurDeDegre);
-		compteurDeDegre=0;
-	}*/
-
-	public  void fermerPince() {
-		Motor.C.setSpeed(500); 
-		Motor.C.backward(); 
-		Delay.msDelay(4000);
-		pinceOuverte=false;
-	}
-
-	public  void ouvrirPince() {
-		Motor.C.setSpeed(500); 
-		Motor.C.forward(); 
-		Delay.msDelay(3000);
-		pinceOuverte=true;
-	}
-
 	public void efficaceTourner(float angle) {
-		compteurDeDegre+=angle;
-		//si +180 // - 180
-		// +90 - 90
-		if(angle<180) //-180
-			//si sur la droite
+		if(angle<180) 
 			tourner(angle);
 		else  {
-			//si doit aller a gauche
 			float nvAngle=360-angle;
 			tourner(-nvAngle);
 		}
 	}
 
-
-	public static void main(String[] args) {
-		Mouvements M = new Mouvements();
-		//M.tourner(360);
-	//	M.ouvrirPince();
-	M.fermerPince();
-		//M.reculer(30);
+	public void tournerVersZoneEnBut() {
+		stopRobot();
+		efficaceTourner(-compteurDeDegre);
+		compteurDeDegre=0;
 	}
+
+	public void fermerPince() {
+		Motor.C.setSpeed(500); 
+		Motor.C.backward(); 
+		pinceOuverte=false;
+		Delay.msDelay(3000);
+	}
+
+	public void ouvrirPince() {
+		Motor.C.setSpeed(500); 
+		Motor.C.forward(); 
+		pinceOuverte=true;
+		Delay.msDelay(3000);
+	}
+
+	public void stopRobot() {
+		Motor.B.flt(true);
+		Motor.D.flt(true);
+		Motor.C.flt(true);
+	}
+	
 }
