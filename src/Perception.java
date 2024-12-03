4af8281387974f9a504e0129ba3a1c4a38ead127
roxdbs <roxane.dubus4@gmail.com>
2024-12-03 11:26:25 +0100

    Update Perception.java
    ajout de la javadoc

--- src/Perception.java
+++ src/Perception.java
@@ -9,34 +9,67 @@ import lejos.robotics.SampleProvider;
 import lejos.robotics.filter.MeanFilter;
 import lejos.utility.Delay;
 
+/**
+ * Une classe qui contient les méthodes pour que le robot perçoive son environnement et,
+ * agisse en conséquence. Par exemple avec les palets, les obstacles (robot adverse) et les murs.
+ */
 public class Perception extends Mouvements{
-	//Capteur ULTRASON
+
+	/**
+	 * Un capteur ultrason.
+	 */
 	private EV3UltrasonicSensor ultrasonicSensor;
 
-	//Capteur COULEUR
+	/**
+	 * Un capteur infrarouge pour percevoir les couleurs des lignes des terrains.
+	 */
 	private EV3ColorSensor colorSensor;
+	/**
+	 * Un attribut sous format RGB pour les couleurs captés.
+	 */
 	private SampleProvider average; 
-	private float DistanceMinPalet; 
-	private final static int vitessederotation = 50;
+	/**
+	 * Un flottant qui prend en valeur la distance du palet le plus proche.
+	 */
+	private float distanceMinPalet; 
+	/**
+	 * Une constante entière qui a pour valeur la vitesse de rotation du robot 
+	 * pour la méthode <code>recherche</code>.
+	 */
+	private final static int VITESSE_DE_ROTATION = 50;
+	/**
+	 * Une constante flottante qui a pour valeur la marge d'erreur acceptée des valeurs RGB de la couleur perçue.
+	 */
 	private final static double ERROR = 0.01;
 
+	/**
+	 * Un constructeur qui indente les capteurs ultrason et infrarouge, 
+	 * l'attribut RGB et qui initialise la distance du palet le plus proche à 0.
+	 */
 	public Perception() {
 		ultrasonicSensor = new EV3UltrasonicSensor(SensorPort.S2);
 		colorSensor = new EV3ColorSensor(SensorPort.S1);
 		average = new MeanFilter(colorSensor.getRGBMode(), 1);
 		colorSensor.setFloodlight(lejos.robotics.Color.WHITE);
-		DistanceMinPalet=0;
+		distanceMinPalet=0;
 	}
-
-	//capte une distance en m
+	
+	/** 
+	 * Une méthode qui retourne la distance de l'objet perçu par le capteur ultrason.
+	 * @return un flottant de la distance en mètres
+	 */
 	public float distance() {
 		SampleProvider distanceProvider = ultrasonicSensor.getDistanceMode(); 
 		float[] sample = new float[distanceProvider.sampleSize()];
 		distanceProvider.fetchSample(sample, 0);
 		return sample[0];
 	}
-
-	//verifie si la distance du palet est la meme que celle quon capte
+	
+	/** 
+	 * Une méthode qui vérifie si la distance du palet le plus proche
+	 * est la même que la distance actuelle perçue par le capteur ultrason.
+	 * @return retourne <b>true</b> si la distance est à peu près la même et <b>false</b> sinon
+	 */
 	public boolean verifierDistance() {
 		float distPerçu = distance();  // Mesure la distance perçu actuelle
 		if (getDistanceMinPalet() == Float.MAX_VALUE) {
@@ -52,12 +85,17 @@ public class Perception extends Mouvements{
 		}
 	}
 
-	//recherche le palet le plus proche et tourner vers ce palet
+	/**
+	 * Une méthode qui cherche la distance du palet le plus proche en tournant sur lui-même de 360° 
+	 * et, qui tourne le robot vers ce palet. 
+	 * La distance du palet le plus proche est stockée dans l'attribut <code>distanceMinPalet</code>.
+	 * @param compteur le nombre entier de distances à percevoir par le capteur ultrason
+	 * @return un flottant de la distance du palet le plus proche
+	 */
 	public float recherche(int compteur) {
-		float []distAng = new float[2];
 
-		Motor.B.setSpeed(vitessederotation);  
-		Motor.D.setSpeed(vitessederotation); 
+		Motor.B.setSpeed(VITESSE_DE_ROTATION);  
+		Motor.D.setSpeed(VITESSE_DE_ROTATION); 
 
 		Motor.B.backward(); 
 		Motor.D.forward(); 
@@ -65,7 +103,7 @@ public class Perception extends Mouvements{
 		int compt = 0; 
 		float[] tabdistance = new float[compteur]; // Tableau pour enregistrer les distances captées
 
-		//scanner son environnement et lenregistrer en tournant 360 d
+		//scanner son environnement et l'enregistrer en tournant 360°
 		while (compt < compteur) {
 			float distance = distance();
 			tabdistance[compt] = distance;
@@ -76,7 +114,7 @@ public class Perception extends Mouvements{
 
 		stopRobot();
 
-		//trouve distance minimale et angle assoscie
+		//trouve distance minimale et angle associe
 		float minDist = Float.MAX_VALUE; //instancie une valeur tres grande
 		int indexMin = 0;
 		for (int i = 0; i < tabdistance.length; i++) {
@@ -88,89 +126,54 @@ public class Perception extends Mouvements{
 			}
 		}
 
-		DistanceMinPalet = minDist;
+		distanceMinPalet = minDist;
 		System.out.println("Distance minimale détectée : " + minDist + " m à l'angle " + indexMin + "°");
 		float angleMin = (360.0f / 220) * indexMin;
 		efficaceTourner(angleMin+5f);
-		return DistanceMinPalet;
+		return distanceMinPalet;
 	}
 
+	/**
+	 * Une méthode qui retourne la valeur de l'attribut <code>distanceMinPalet</code>.
+	 * @return la valeur flottante de l'attribut 
+	 */
 	public float getDistanceMinPalet() {
-		return this.DistanceMinPalet;
+		return this.distanceMinPalet;
 	}
 
-	/*
-	//verifier si la distance captée sur le moment est egale à la distance minimale
-	public boolean verifier(float[] distances, int index, int debut, int fin, float seuil) {
-	    int taille = distances.length;
-
-	    for (int i = index + debut; i <= index + fin; i++) {
-	        if (i < 0 || i >= taille) continue; // si hors limites
-	        if (Math.abs(distances[i] - distances[index]) > seuil) {
-	            return false; // c'est pas un mur
-	        }
-	    }
-
-	    return true; // c'est un mur
-	}
+	/**
+	 * Une méthode qui éteint le capteur ultrason.
 	 */
-
-	/*
-	 //si il y a un mur ne pas prendre en compte
-	public boolean estMur(float[] distances, int index, float seuil) {
-	    // differents possibiltes
-	    if (verifier(distances, index, -10, 0, seuil)) {
-	        return true; // Les 10 distances avant sont proches
-	    }
-	    if (verifier(distances, index, 0, 10, seuil)) {
-	        return true; // Les 10 distances après sont proches
-	    }
-	    if (verifier(distances, index, -5, 5, seuil)) {
-	        return true; 
-	    }
-	    if (verifier(distances, index, -3, 7, seuil)) {
-	        return true; 
-	    }
-	    if (verifier(distances, index, -2, 8, seuil)) {
-	        return true; 
-	    }
-	    if (verifier(distances, index, -8, 2, seuil)) {
-	        return true; 
-	    }
-	    if (verifier(distances, index, -7, 3, seuil)) {
-	        return true; 
-	    }
-	    return false; // Si aucune de ces conditions ne sont valides
-	}
-	 */
-
 	public void close() {
 		ultrasonicSensor.close(); // eteint le capteur
 	}
 
-	// Méthode pour détecter si le robot est sur une couleur similaire à la blanche
+	/**
+	 * Une méthode qui détecte si le capteur infrarouge perçoit la couleur en paramètre 
+	 * selon une marge d'erreur pour les valeurs RGB.
+	 * @param couleurReference les valeurs RGB de la couleur
+	 * @return retourne <b>true</b> si le robot est sur cette couleur et <b>false</b> sinon
+	 */
 	public boolean surCouleur(float[] couleurReference) {
 		float[] couleurActuelle = new float[average.sampleSize()];
 		average.fetchSample(couleurActuelle, 0);
 
 		// Calculer la différence scalaire entre la couleur actuelle et la couleur de référence
 		double scalaireDifference = scalaire(couleurActuelle, couleurReference);
-		System.out.println("Scalaire: " + scalaireDifference);
 
 		// Retourne vrai si la différence est inférieure à l'erreur acceptée
 		return scalaireDifference < ERROR;
 	}
 
-	// Fonction scalaire pour comparer deux couleurs (RGB)
+	/**
+	 * Une méthode qui calcule la différence deux couleurs selon leurs valeurs RGB.
+	 * @param v1 les valeurs RGB de la couleur 1
+	 * @param v2 les valeurs RGB de la couleur 2
+	 * @return la valeur flottante de la différence des valeurs RGB des deux couleurs
+	 */
 	public static double scalaire(float[] v1, float[] v2) {
 		return Math.sqrt(Math.pow(v1[0] - v2[0], 2.0) +
 				Math.pow(v1[1] - v2[1], 2.0) +
 				Math.pow(v1[2] - v2[2], 2.0));
 	}
-
-	public static void main(String[] args) {
-		Perception p = new Perception();
-		p.recherche(220);
-	}
-
 }
